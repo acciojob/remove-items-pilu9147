f01bfda37c1a17fe87b07283aeb932940e118d0b
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 16:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+
 //your JS code here. If required.
+function deleteElement() {
+	let sele = document.getElementsByTagName("select")[0];
+	let val = sele.value;
+	let child = sele.children;
+	for(let i = 0;i < sele.length;i++) {
+		if(val===child[i].value) {
+		 child[i].remove()
+		}
+	}
+}
+// function deleteElement() {
+//     var selectElement = document.getElementById('colorSelect');
+//     var selectedColor = selectElement.value;
+
+//     for (var i = 0; i < selectElement.options.length; i++) {
+//         if (selectElement.options[i].value === selectedColor) {
+//             selectElement.remove(i);
+//             break;
+//         }
+//     }
+// }
